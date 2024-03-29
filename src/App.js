c082eb6bd2395ad7da3b24376a9487790aa5262c
arum <arumshin.dev@gmail.com>
2024-03-29 19:55:57 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useState} from "react";
 
 function App() {
+    const [counter, setValue] = useState(0);
+    const onClick = () => setValue((prev)=> prev+1);
   return (
     <div>
-      <h1 className={styles.title}>초기 세팅</h1>
+        <h1 className={styles.title}>초기 세팅</h1>
         <Button text={"Continue"}/>
+        <h2>{counter}</h2>
+        <button onClick={onClick}>Click me</button>
+
     </div>
   );
 }
