35ffd3607b05734c764e9b110fecd2e6e48c3e9e
arum <arumshin.dev@gmail.com>
2024-03-30 20:17:24 +0900

    useEffect-cleanup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import {useState, useEffect} from "react";
 
+function Hello(){
+    useEffect(() => {
+        console.log("created:D");
+        return () => {console.log("destroyed:X");}
+        },[]);
+    return <h1>Hello</h1>;
+}
 function App() {
+    const [showing, setShowing] = useState(false);
+    const onShowing = () => setShowing((prev) => !prev);
     const [counter, setValue] = useState(0);
     const [keyword, setKeyword] = useState("");
     const onClick = () => setValue((prev) => prev + 1);
@@ -25,9 +34,13 @@ function App() {
     <div>
         <h1 className={styles.title}>초기 세팅</h1>
         <Button text={"Continue"}/>
+        <hr/>
         <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
         <h2>{counter}</h2>
         <button onClick={onClick}>Click me</button>
+        <hr/>
+        {showing ? <Hello /> : null}
+        <button onClick={onShowing}>{showing ? "Hide" : "Show"}</button>
 
     </div>
   );
