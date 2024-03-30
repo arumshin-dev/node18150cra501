4ece9a4acb912ce8c86ab8efa921d8a47d4da44c
arum <arumshin.dev@gmail.com>
2024-03-30 19:46:14 +0900

    useEffect-deps

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import {useState, useEffect} from "react";
 
 function App() {
     const [counter, setValue] = useState(0);
-    const onClick = () => setValue((prev)=> prev+1);
-    console.log("i run all time");
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
+    console.log("i run all the time");
     const iRunOnlyOnce = () => {
         console.log("i run only once");
     }
     useEffect(iRunOnlyOnce, []);
+    useEffect(() => {
+        console.log("I run when 'keyword' changes, Search for", keyword);
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run when 'counter' changes.", counter);
+    }, [counter]);
   return (
     <div>
         <h1 className={styles.title}>초기 세팅</h1>
         <Button text={"Continue"}/>
+        <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
         <h2>{counter}</h2>
         <button onClick={onClick}>Click me</button>
 
