89cd5ff7a4f48ab3c9b3b5f0c3a528d873867b9e
arum <arumshin.dev@gmail.com>
2024-03-31 18:28:39 +0900

    setToDos

--- src/App.js
+++ src/App.js
@@ -1,47 +1,25 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import {useState, useEffect} from "react";
+import {useState} from "react";
 
-function Hello(){
-    useEffect(() => {
-        console.log("hi:)");
-        return () => {console.log("bye:(");}
-        },[]);
-    return <h1>Hello</h1>;
-}
 function App() {
-    const [showing, setShowing] = useState(false);
-    const onShowing = () => setShowing((prev) => !prev);
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-    console.log("i run all the time");
-    const iRunOnlyOnce = () => {
-        console.log("i run only once");
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (e)=> setToDo(e.target.value);
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        if(toDo ===""){
+            return;
+        }
+        setToDos(currentArray => [toDo, ...currentArray]);
+        setToDo("");
     }
-    useEffect(iRunOnlyOnce, []);
-    useEffect(() => {
-        console.log("I run when 'keyword' changes, Search for", keyword);
-    }, [keyword]);
-    useEffect(() => {
-        console.log("I run when 'counter' changes.", counter);
-    }, [counter]);
-    useEffect(() => {
-        console.log("I run when keyword & counter changes.");
-    }, [keyword,counter]);
+    console.log(toDos)
   return (
     <div>
-        <h1 className={styles.title}>초기 세팅</h1>
-        <Button text={"Continue"}/>
-        <hr/>
-        <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
-        <h2>{counter}</h2>
-        <button onClick={onClick}>Click me</button>
-        <hr/>
-        {showing ? <Hello /> : null}
-        <button onClick={onShowing}>{showing ? "Hide" : "Show"}</button>
-
+        <h1>My To Dos ({toDos.length})</h1>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
+            <button>Add To Do</button>
+        </form>
     </div>
   );
 }
