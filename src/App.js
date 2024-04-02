97f41cc639da056d47d7c51627d7dcc961da639a
arum <arumshin.dev@gmail.com>
2024-04-02 20:12:05 +0900

    list_movies fetch api

--- src/App.js
+++ src/App.js
@@ -1,73 +1,20 @@
 import {useEffect, useState} from "react";
 
 function App() {
-    //coin 정보 가져오기
     const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]);
-    useEffect(() => {
-        fetch("https://api.coinpaprika.com/v1/tickers")
-            .then((response) => response.json())
-            .then((json) => {
+    const [movies, setMovies] = useState([]);
+    useEffect(()=>{
+        fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&language=ko-KR&page=1`)
+            .then((response)=>response.json())
+            .then((json)=>{
                 //console.log(json);
-                setCoins(json);
-                setLoading(false);
+                setMovies(json.data.movies);
             });
-    }, []);
-    //코인 선택
-    const [index, setIndex] = useState("xx");
-    const onSelect = (event) =>{
-        //console.log(event.target.value);
-        //console.log(event.target.key);
-        //console.log(event.target.selectedIndex);
-        //console.log(coins[event.target.selectedIndex-1]);
-        setIndex(event.target.value);
-    };
-
-    //코인 선택하면 몇개 살수 있는지 계산
-    function Converter(prop) {
-        console.log(prop.coin);
-        //숫자 변수 생성
-        const [amount, setAmount] = useState(0);//얼마 인풋 값
-        const [result, setResult] = useState(0);//코인 몇개 계산 값
-        const buyCoin = (event) => {
-            //이벤트에서 input에 value 가져오기
-            setAmount(event.target.value);
-            setResult(event.target.value / prop.coin.quotes.USD.price);
-        }
-        return (
-        <>
-            <p>{prop.coin.name}({prop.coin.symbol}):${prop.coin.quotes.USD.price}</p>
-        <label htmlFor="money">USD</label>
-        <input id="money"
-               placeholder="money USD"
-               type="number"
-               value={amount}
-               onChange={buyCoin}
-        />
-            <h1>{result}개 살수 있다</h1>
-        </>
-        )
-    }
-
+    },[]);
+    console.log(movies);
     return (
         <div>
-            <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-
-            {loading ? <strong>Loading...</strong> : (
-                <>
-                    <select value={index} onChange={onSelect}>
-                        <option key="xx" value="xx">select your coin</option>
-                        {coins.map((coin, index) => {
-                            //console.log(index);
-                            return <option key={index} value={index}>{coin.name} ({coin.symbol}) :
-                                ${coin.quotes.USD.price} USD</option>
-                        })}
-                    </select>
-                    <hr/>
-                    {index ==="xx" ? "Please select coin"
-                        : <Converter coin={coins[index]}/>}
-                </>
-            )}
+            {loading ? <h1>Loading...</h1> : null}
         </div>
     );
 }
