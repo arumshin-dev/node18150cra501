ae6aa8920b34e594fd88ade5e1670c27fb748e7f
arum <arumshin.dev@gmail.com>
2024-04-02 20:21:12 +0900

    list_movies fetch api async-await

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import {useEffect, useState} from "react";
 function App() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&language=ko-KR&page=1`);
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    }
     useEffect(()=> {
-        fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&language=ko-KR&page=1`)
+        getMovies();
+        /*fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&language=ko-KR&page=1`)
             .then((response)=>response.json())
             .then((json)=> {
                 //console.log(json);
                 setMovies(json.data.movies);
                 setLoading(false);
-            });
+            });*/
     },[]);
     console.log(movies);
     return (
