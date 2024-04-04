db9e488975d9dd15b63124fa68c9fed110a56330
arum <arumshin.dev@gmail.com>
2024-04-04 20:53:34 +0900

    Router Routes Route

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
-import {useEffect, useState} from "react";
-import Movie from "./Movie";
+// import {useEffect, useState} from "react";
+// import Movie from "./components/Movie";
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const json = await (
-            await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&language=ko-KR&page=1`)
-        ).json();
-        //const json = await response.json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    }
-    useEffect(()=> {
-        getMovies();
-        /*fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&language=ko-KR&page=1`)
-            .then((response)=>response.json())
-            .then((json)=> {
-                //console.log(json);
-                setMovies(json.data.movies);
-                setLoading(false);
-            });*/
-    },[]);
-    console.log(movies);
-    return (
-        <div>
-            {loading ? <h1>Loading...</h1>
-                : <div>{movies.map((movie)=>(
-                    <Movie
-                        key={movie.id}
-                        coverImg={movie.medium_cover_image}
-                        title={movie.title}
-                        summary={movie.summary}
-                        genres={movie.genres}
-                    />
-                ))}</div>}
-        </div>
-    );
+    return (<Router>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+        </Routes>
+    </Router>)
 }
 
 export default App;
