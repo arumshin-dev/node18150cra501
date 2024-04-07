082c9775c221d8d1ac7f60e4a1cb1d1a777c6677
arum <arumshin.dev@gmail.com>
2024-04-07 19:58:02 +0900

    movie - year 추가

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 
-function Movie({id, coverImg, title, summary, genres}) {
+function Movie({id, coverImg, title, year, summary, genres}) {
     return (
         <div>
             <img src={coverImg} alt={title}/>
             <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
+            <h3>{year}</h3>
             <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
             <ul>
                 {genres.map(g => (
